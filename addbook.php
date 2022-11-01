c866047d67416bcd0a24ec788dc92cde2393bf28
Nouf Alsadhan <noufysd01@gmail.com>
2022-11-01 19:51:50 +0300

    add to cart

---
+++ addbook.php
@@ -0,0 +1,23 @@
+<?php include 'connectDB.php';?>
+
+<?php
+$sql1="Select * from book where isbn=".$_GET['isbn'];
+$result1=mysqli_query($link,$sql1);
+$row1=mysqli_fetch_assoc($result1);
+addItem($_GET['isbn'],$row1['name'],$row1['price'],$row1['image']);
+                
+         
+                function addItem($isbn,$name,$price,$image) {
+                    
+                    global $link;
+                    
+                 $sql="INSERT INTO cart VALUES ('$isbn', '$name','$price','$image')";
+    $result = mysqli_query($link, $sql);
+   
+   header('Location: ' . $_SERVER['HTTP_REFERER']);
+                    
+                }
+                
+
+
+?>
