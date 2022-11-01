d7f2d25d1edb53e7122abee6c844971dcc7ff73b
Nouf Alsadhan <noufysd01@gmail.com>
2022-11-01 20:36:43 +0300

    update

--- searchall.php
+++ searchall.php
@@ -11,34 +11,25 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
         <title>Search</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/style.css" type="text/css" rel="stylesheet">
+        <link href="css/s.css" type="text/css" rel="stylesheet">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <script><!-- comment -->
-           if ( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href+"?genre=All" );
-} 
+
             </script>
     </head>
     <body>
        
            
-               
-      <!--<button type="submit" class="btn btn-primary btn-sm float-end">Search</button>-->
+
                            
        <div id="h"><img src="img/logo.png" height=80 width=160><a id="cart" href='cart.php'> <i class="fa fa-shopping-cart" ></i></a></div><hr>
        
 
        
        <form action="searchall.php" method="POST">
-
-          <!--<button onclick="myfunction()" class="genre" name="n">NOVELS</button>
-          <button onclick="myfunction()" class="genre" name="m">MYSTERY</button>
-          <button onclick="myfunction()" class="genre" name="C">CRIME</button>
-          <button onclick="myfunction()" class="genre" name="E">EDUCATION</button>
-          <button onclick="myfunction()" class="genre" name="A">ALL</button><hr>-->
           
-           <button onclick="myfunction()" class="genre" name='n'>NOVELS</button><button onclick="myfunction()" name='m' class="genre">MYSTERY</button><button onclick="myfunction()" name='C' class="genre">CRIME</button><button onclick="myfunction()" name='E' class="genre">EDUCATION</button><button name='A' onclick="myfunction()" class="genre">ALL</button><hr>
+           <button class="genre1" onclick="myfunction()"  name='n'>NOVELS</button><button onclick="myfunction()" name='m' class="genre1">MYSTERY</button><button onclick="myfunction()" name='C' class="genre1">CRIME</button><button onclick="myfunction()" name='E' class="genre1">EDUCATION</button><button name='A' onclick="myfunction()" class="genre1">ALL</button><hr>
            </form>
            
 
@@ -77,7 +68,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this
              
                $result = mysqli_query($link, $sqlj);
                if(mysqli_num_rows($result)==0){
-                   echo"<br><br><h1>There are books under this genre";
+                   echo"<br><br><h1>There are no books under this genre";
         echo"<br><br><br><br><br><br><br><br><br><br><br><br><hr><hr>";
         echo"</h1>";
                }
